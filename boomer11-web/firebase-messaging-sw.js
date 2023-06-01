310d61af2bc11398d5daf2d75f122b9e3645669e
logeshwaran <logeshwaran.s@sciflare.com>
2023-06-01 11:39:11 +0530

    latest

---
+++ boomer11-web/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js");
+firebase.initializeApp({
+    apiKey: "AIzaSyA-MHvPCAm_7wqgk7j5IWJvQ3w46JQsEds",
+    authDomain: "push-notification-b4484.firebaseapp.com",
+    projectId: "push-notification-b4484",
+    storageBucket: "push-notification-b4484.appspot.com",
+    messagingSenderId: "937201699226",
+    appId: "1:937201699226:web:a3db03b7e7d9642440d223",
+    measurementId: "G-74CTER88FP"
+});
+const messaging = firebase.messaging();
